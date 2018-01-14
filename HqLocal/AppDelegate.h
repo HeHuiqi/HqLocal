eb5e8bfd9ad35e2e6ff19c878035cbcfcc1286e2
hehuiqi <253956882@qq.com>
2018-01-14 18:28:02 +0800

    Initial Commit

---
+++ HqLocal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HqLocal
+//
+//  Created by hehuiqi on 2018/1/14.
+//  Copyright © 2018年 hehuiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
